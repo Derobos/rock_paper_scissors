8c35449f08b3bb67c2f4dab947215b12f5656cdc
Derobos <Derobos@outlook.com>
2021-02-12 13:16:29 -0600

    Completed this rock paper scissors game.  Added a ui and styling.

--- readMe.md
+++ readMe.md
@@ -1,48 +1,5 @@
-Javascript assignment from Theodinproject.com
+Rock paper scissors excersize from theOdinProject.
 
- var items = ["ROCK", "PAPER", "SCISSORS"];
-        var userScore = 0;
-        var compScore = 0;
-
-
-        function computerPlay() {
-            return items[Math.floor(Math.random() * items.length)];
-        }
-
-
-
-        function round(userPlay, compPlay) {
-
-            var userPlay = prompt("please choose ROCK, PAPER, or SCISSORS!", "ROCK").toUpperCase();
-            var compPlay = computerPlay().toUpperCase();
-            console.log("Computer chose " + compPlay);
-            if (userPlay == compPlay) {
-                console.log("Tie");
-
-            } else if (
-                (userPlay == "ROCK" && compPlay == "PAPER") ||
-                (userPlay == "PAPER" && compPlay == "SCISSORS") ||
-                (userPlay == "SCISSORS" && compPlay == "ROCK")
-            ) {
-                compScore++;
-                console.log("Computer Wins!");
-            } else {
-                userScore++;
-                console.log("You Win!");
-            }
-            var score = ("Your Score " + userScore + "  " + "Computer Score " + compScore);
-            console.log(score);
-        }
-
-        function game() {
-
-            var turn;
-            for (turn = 0; turn < 5; turn++) {
-                round();
-            }
-            if (userScore > compScore) {
-                Console.log("You win the game!");
-            } else {
-                console.log("You Lose the game!");
-            }
-        }
+<button class="btns" id="ROCK">Rock</button>
+        <button class="btns" id="PAPER">Paper</button>
+        <button class="btns" id="SCISSORS">Scissors</button>
--- script.js
+++ script.js
@@ -1,52 +1,84 @@
-/*begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play.*/
-var items = ["ROCK", "PAPER", "SCISSORS"];
+//Set playfield and buttons
+const container = document.querySelector('.playField');
+const controls = document.createElement('div');
+controls.classList.add('controls');
+container.appendChild(controls);
+
+
+const btn1 = document.getElementById('#ROCK');
+
+const btn2 = document.getElementById('#PAPER');
+
+const btn3 = document.getElementById('#SCISSORS');
+
+
+
+
+//create the game components and explain the game
+const btns = document.querySelectorAll('.btns');
+const items = ["ROCK", "PAPER", "SCISSORS"];
 var userScore = 0;
 var compScore = 0;
+document.getElementById("score").innerHTML = "Rock beats Scissors<br> Scissors beats Paper<br> Paper beats rock<br> First to 5 points wins<br> Choose one to begin!";
 
 
+// sets computers choice
 function computerPlay() {
     return items[Math.floor(Math.random() * items.length)];
-}
+};
+
+// player selection starts the game and records a score for each round played.
+
+btns.forEach((button) => button.addEventListener('click', function play() {
 
 
-/*The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round, Make your function case insensitive*/
+    var userPlay = button.id;
+    var compPlay = computerPlay();
 
 
-
-function round(userPlay, compPlay) {
-
-    var userPlay = prompt("please choose ROCK, PAPER, or SCISSORS!", "ROCK").toUpperCase();
-    var compPlay = computerPlay().toUpperCase();
     console.log("Computer chose " + compPlay);
     if (userPlay == compPlay) {
         console.log("Tie");
+        document.getElementById("score").innerHTML = "You chose: " + userPlay + "<br>Computer chose: " + compPlay + "<br><h1>It's a tie!</h1>";
+
 
     } else if (
         (userPlay == "ROCK" && compPlay == "PAPER") ||
         (userPlay == "PAPER" && compPlay == "SCISSORS") ||
         (userPlay == "SCISSORS" && compPlay == "ROCK")
     ) {
-        compScore++;
+
         console.log("Computer Wins!");
+        document.getElementById("score").innerHTML = "You chose: " + userPlay + "<br>Computer chose: " + compPlay + "<br><br><h1>Computer wins!</h1>";
+        compScore++;
     } else {
-        userScore++;
+
         console.log("You Win!");
+        document.getElementById("score").innerHTML = "You chose: " + userPlay + "<br>Computer chose: " + compPlay + "<br><br><h1>You win!</br>";
+        userScore++;
     }
     var score = ("Your Score " + userScore + "  " + "Computer Score " + compScore);
+    game();
+
+    const uSc = document.querySelector('#uSc');
+    document.getElementById("uSc").innerHTML = "You: " + userScore;
+    const cSc = document.querySelector('cSc');
+    document.getElementById("cSc").innerHTML = "Computer: " + compScore;
     console.log(score);
-}
+
+}));
 
 /*Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.*/
 
 function game() {
 
-    var turn;
-    for (turn = 0; turn < 5; turn++) {
-        round();
-    }
-    if (userScore > compScore) {
-        console.log("You win the game!");
-    } else {
-        console.log("You Lose the game!");
-    }
-}
+    if (userScore == 5) {
+        alert("You win the game! " + userScore + " - " + compScore + " Try again?");
+        userScore = 0;
+        compScore = 0;
+    } else if (compScore == 5) {
+        alert("You Lose the game! " + compScore + " - " + userScore + " Try again?");
+        userScore = 0;
+        compScore = 0;
+    } else console.log("First to 5 points wins!");
+};
